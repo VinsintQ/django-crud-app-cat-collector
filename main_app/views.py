0c6306ea5c6097872c7266486451ac9d57deb7a2
Mueen <hejiy2002@gmail.com>
2024-08-29 11:03:05 +0300

    adding images and some styles

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello ᓚᘏᗢ</h1>")
+    return render(request, "base.html")
 
 
 def about(request):
